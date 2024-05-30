bc4b6c2802aff669a0d22c4f849b5c47167a1e24
Miguel González <152456005+Miguel-Gonzalez01@users.noreply.github.com>
2024-05-30 06:31:44 +0000

    Terminado

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -1,6 +1,7 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
+			token: null,
 			message: null,
 			demo: [
 				{
@@ -21,10 +22,56 @@ const getState = ({ getStore, getActions, setStore }) => {
 				getActions().changeColor(0, "green");
 			},
 
+			syncTokenFromSessionStore: () => {
+				const token = sessionStorage.getItem("token");
+				console.log("App just loaded, synching the session storage token")
+				if(token && token !="" && token != undefined) setStore({ token: token });
+			},
+			logout: () => {
+				sessionStorage.removeItem("token");
+				console.log("Log out")
+				setStore({ token: null });
+			},
+			login: async (email, password) => {
+				const opts = {
+					method: 'POST',
+					headers: {
+						"Content-Type": "application/json"
+					},
+					body: JSON.stringify({
+						"email": email,
+						"password": password
+					})
+				}
+
+				try{
+					const resp = await fetch('https://orange-space-goggles-wrrg6j9gxqxr29j99-3001.app.github.dev/api/token', opts)
+					if(resp.status === 200){
+						alert("There has been some error");
+						return false;
+					} 
+
+					const data = await resp.json();
+					console.log("this came from the backend", data);
+					sessionStorage.setItem("token", data.access_token);
+					setStore({ token: data.access_token })	
+					return true;
+				}
+				catch(error){
+					console.error("there has been an error loggin in")
+				}
+			},
+
 			getMessage: async () => {
+				const store = getStore();
+				const opts = {
+					headers: {
+						"Authorization": "Bearer " + store.token
+					}
+				}
 				try{
 					// fetching data from the backend
-					const resp = await fetch(process.env.BACKEND_URL + "/api/hello")
+					const resp = await fetch("https://orange-space-goggles-wrrg6j9gxqxr29j99-3001.app.github.dev", opts)
 					const data = await resp.json()
 					setStore({ message: data.message })
 					// don't forget to return something, that is how the async resolves
